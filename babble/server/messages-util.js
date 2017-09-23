4efb9d113bbbacf231ab76df5c2e72dbbd52b935
SherenM <sherenm02@gmail.com>
2017-09-23 17:47:09 +0300

    Create messages-util.js

---
+++ babble/server/messages-util.js
@@ -0,0 +1,58 @@
+"use strict";
+
+//adding two arrays to the global variable Babble 
+var Babble = {
+    serverCounter: 0,
+    lastMsgId: 0,
+    usersList: [],
+    messagesList: []
+}
+
+
+//you can know the values of the room stats by the length of each array above using: Babble.users.length;
+
+function addMessage(message) {    //the function return Number(id)
+    Babble.serverCounter = Babble.serverCounter + 1;
+    var gravatar,newMessage;
+    //get the image of the user
+    if (message.email === "" || message.email === undefined) {
+        gravatar = "/client/images/anonymous.png";
+    } else {
+        var userIndex = Babble.usersList.findIndex(function (element) {
+            return element.email === message.email;
+        });
+        gravatar = Babble.usersList[userIndex].gravatar;
+    }
+    
+    newMessage = {
+        name: message.name,
+        email: message.email,
+        gravatar: gravatar,
+        message: message.message,
+        timestamp: message.timestamp,
+        id: Babble.serverCounter
+    }
+    Babble.messagesList.push(newMessage);
+    Babble.lastMsgId = Math.max.apply(Math, Babble.messagesList.map(function (o) { return o.id; })); //will return the newst id number 
+    return Babble.serverCounter;
+}
+function getMessages(counter) {
+    var messages = [];
+
+    messages = Babble.messagesList.filter(function (element) {
+        return element.id > counter;
+    });
+    //console.log("msgs : " + JSON.stringify(messages));
+    return messages;
+}
+function deleteMessage(id) {  //pay attention,id is string***********
+    var idIndex = Babble.messagesList.findIndex(function (element) {
+        return element.id == id;
+    });
+    console.log("idIndex : " + idIndex);
+  //  if (idIndex >= 0) {
+        Babble.messagesList.splice(idIndex, 1);
+    //}
+    Babble.lastMsgId = Math.max.apply(Math, Babble.messagesList.map(function (o) { return o.id; })); //will return the newst id number 
+}
+module.exports = { Babble, addMessage, getMessages, deleteMessage };
